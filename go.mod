4569924d62771f9fc4c9d1d3ff0e735246625ca2
cheezecakee <anabel.pmoraes@gmail.com>
2025-07-22 22:17:42 -0300

    config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/cheezecakee/eww-notify-go
 
 go 1.24.4
+
+require (
+	github.com/BurntSushi/toml v1.5.0
+	github.com/pelletier/go-toml/v2 v2.2.4
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
 var DefaultConfig = Config{
 	EwwDefaultNotificationKey: nil,
 	EwwWindow:                 nil,
@@ -15,7 +23,7 @@ var DefaultConfig = Config{
 }
 
 type ConfigFile struct {
-	Config Config
+	Config Config `toml:"config"`
 }
 
 type Config struct {
@@ -53,4 +61,71 @@ func (o *Orientation) UnmarshalText(text []byte) error {
 		*o = Vertical
 	}
 	return nil
+}
+
+func GetConfigDir() (string, error) {
+	configDir := os.Getenv("XDG_CONFIG_HOME")
+	if configDir == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to get user home directory: %w", err)
+		}
+		configDir = filepath.Join(homeDir, ".config")
+	}
+	return configDir, nil
+}
+
+func LoadConfig() (*Config, error) {
+	configDir, err := GetConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get config directory: %w", err)
+	}
+
+	configFilePath := filepath.Join(configDir, "end", "config.toml")
+
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		fmt.Printf("Could not find config file! Should be at %s\n", configFilePath)
+		return nil, nil
+	}
+
+	configData, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var configFile ConfigFile
+
+	configFile.Config = DefaultConfig
+
+	if err := toml.Unmarshal(configData, &configFile); err != nil {
+		fmt.Println("There were errors in your config.toml!")
+		fmt.Printf("Error: %v\n", err)
+		return nil, nil
+	}
+
+	mergedConfig := mergeWithDefaults(configFile.Config)
+	return &mergedConfig, nil
+}
+
+func mergeWithDefaults(cfg Config) Config {
+	result := cfg
+
+	if result.Timeout.ByUrgency.Low == 0 &&
+		result.Timeout.ByUrgency.Normal == 0 &&
+		result.Timeout.ByUrgency.Critical == 0 {
+		result.Timeout = DefaultConfig.Timeout
+	} else {
+		if result.Timeout.ByUrgency.Low == 0 {
+			result.Timeout.ByUrgency.Low = DefaultConfig.Timeout.ByUrgency.Low
+		}
+		if result.Timeout.ByUrgency.Normal == 0 {
+			result.Timeout.ByUrgency.Normal = DefaultConfig.Timeout.ByUrgency.Normal
+		}
+	}
+
+	if result.NotificationOrientation == "" {
+		result.NotificationOrientation = DefaultConfig.NotificationOrientation
+	}
+
+	return result
 }
